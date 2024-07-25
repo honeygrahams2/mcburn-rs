4b6f65691a8d0e69855fdd7786df519fc1f66134
Helen Graham <23189289+honeygrahams2@users.noreply.github.com>
2024-07-24 23:25:19 -0400

    modified:   Cargo.lock 	modified:   Cargo.toml 	new file:   SECURITY.md 	modified:   src/lib.rs

---
+++ SECURITY.md
@@ -0,0 +1,18 @@
+# Security Policy
+
+<a name="reporting"></a>
+## Reporting security problems in the McBurn Program
+
+**DO NOT CREATE A GITHUB ISSUE** to report a security problem.
+
+Instead please contact the maintainers at one of the following locations:
+
+* Email: nathan@airadlabs.com
+* Twitter: @SolDapper
+
+<a name="process"></a>
+## Incident Response Process
+
+Maintainers will respond to security incidents as fast as possible, and will keep you informed of our progress.
+
+The McBurn program on Solana permanently burns a cNFT asset.
--- src/lib.rs
+++ src/lib.rs
@@ -8,4 +8,19 @@ pub mod state;
 use include_idl::include_idl;
 
 #[cfg(not(feature = "no-entrypoint"))]
+use solana_security_txt::security_txt;
+
+#[cfg(not(feature = "no-entrypoint"))]
 include_idl!();
+
+#[cfg(not(feature = "no-entrypoint"))]
+security_txt! {
+    // Required fields
+    name: "McBurn",
+    project_url: "https://https://github.com/honeygrahams2/mcburn-rs",
+    contacts: "email:nathan@airadlabs.com",
+    policy: "https://github.com/honeygrahams2/mcburn-rs/SECURITY.md",
+
+    // Optional Fields
+    source_code: "https://https://github.com/honeygrahams2/mcburn-rs"
+}
